fab6e5ea67d4ccd3a754c4db93b838e71ce6a653
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2021-11-17 04:38:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+name1_lowercase = name1.lower()
+name2_lowercase = name2.lower()
+
+
+
