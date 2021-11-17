a12ee2b47340ef092c21d7089bf07bd6fc6f3ad1
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2021-11-17 04:58:09 +0000

    Converted name to lowercases and count the amount of letter in every letter of the string "TRUE LOVE". Also the 'love_score' variable was created and converted to integer, after concatenated the result(not sum) of TRUE and LOVE separately.

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ name2 = input("What is their name? \n")
 name1_lowercase = name1.lower()
 name2_lowercase = name2.lower()
 
+name_count_t = name1_lowercase.count("t") + name2_lowercase.count("t")
+name_count_r = name1_lowercase.count("r") + name2_lowercase.count("r")
+name_count_u = name1_lowercase.count("u") + name2_lowercase.count("u")
+name_count_e = name1_lowercase.count("e") + name2_lowercase.count("e")
+name_true_count = name_count_t + name_count_r + name_count_u + name_count_e
+name_count_l = name1_lowercase.count("l") + name2_lowercase.count("l")
+name_count_o = name1_lowercase.count("o") + name2_lowercase.count("o")
+name_count_v = name1_lowercase.count("v") + name2_lowercase.count("v")
+name_love_count = name_count_l + name_count_o + name_count_v + name_count_e
+
+love_score = int(f'{name_true_count}{name_love_count}')
 
 
